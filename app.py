cc907ae1494c1ba5884ae90bbf81ed182e5389f8
mhasa <m.hasan09@yahoo.com>
2025-06-13 00:47:10 +0500

    Correct the data jsonfy

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 def table():
     """Main dashboard page."""
     try:
-        # stream_data = get_stream_data()
         stream_data = get_stream_data()
+        # stream_data = get_stream_data_dummy()
         return render_template('index.html',
                                stream_data=stream_data,
                                )
@@ -22,8 +22,8 @@ def table():
 def get_data():
     """API endpoint to get data via AJAX."""
     try:
-        # stream_data = get_stream_data()
-        stream_data = get_stream_data_dummy()
+        stream_data = get_stream_data()
+        # stream_data = get_stream_data_dummy()
         return jsonify(stream_data)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
